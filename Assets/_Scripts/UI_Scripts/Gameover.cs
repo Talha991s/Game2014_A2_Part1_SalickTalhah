183414d0a293db28da06ff9c70501316271bb60e
Talha991s <55731371+Talha991s@users.noreply.github.com>
2020-11-20 13:18:31 -0800

    Added the GameOver  Scene and functionality.

---
+++ Assets/_Scripts/UI_Scripts/Gameover.cs
@@ -0,0 +1,26 @@
+﻿/*
+ Filename: MainMenu.cs
+ Author: Salick Talhah
+ Student Number: 101214166
+ Date last modified: 20/11/2020
+ Description: This file have everything for the Game Over Scene.
+ Revision History:
+ 20/11/2020
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Gameover : MonoBehaviour
+{
+    public void TryAgain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    public void BackToMain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+}
