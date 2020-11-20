66a616b0a5659fca61d1e3d1a1784f012c822238
Talha991s <55731371+Talha991s@users.noreply.github.com>
2020-11-20 12:44:47 -0800

    Added Main Menu and Functionality, Added more Assets.

---
+++ Assets/_Scripts/UI_Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿/*
+ Filename: MainMenu.cs
+ Author: Salick Talhah
+ Student Number: 101214166
+ Date last modified: 20/11/2020
+ Description: This file load the playscene and have the function for the buttons in the Mainmenu.
+ Revision History:
+ 20/11/2020
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    //mainmenu function
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
